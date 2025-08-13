2ac9d807c2f71dcda9fcc06c93deff409bd4b154
Muberra Bakmaz <muberrabakmaz@icloud.com>
2025-08-13 15:01:21 +0300

    daire değiştirildi

--- src/giriş/daire.java
+++ src/giriş/daire.java
@@ -2,16 +2,16 @@ package giriş;
 import java.util.Scanner;
 
 public class daire {
-    public static void main(String[] args){
-        int r;
-        double pi=3.14;
-        double alan,cevre;
-        Scanner input=new Scanner(System.in);
-        System.out.print("dairenin yarıçapını giriniz:");
-        r= input.nextInt();
-        alan=pi*r*r;
-        cevre=2*pi*r;
-        System.out.println("dairenin alanı:"+alan);
-        System.out.print("dairenin çevresi:"+cevre);
+    public static void main(String[] args) {
+        int r, aci;
+        double pi = 3.14;
+        double alan;
+        Scanner input = new Scanner(System.in);
+        System.out.println("dairenin yarıçapını giriniz:");
+        r = input.nextInt();
+        System.out.println("dairenin merkez açısını giriniz:");
+        aci = input.nextInt();
+        alan = (pi * (r * r) * aci) / 360;
+        System.out.print("dairenin alanı:" + alan);
     }
 }

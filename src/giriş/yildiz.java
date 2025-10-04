0de1e33be960a43daca92e4e7e40c3cad6a60e34
Muberra Bakmaz <muberrabakmaz@icloud.com>
2025-10-04 22:20:18 +0300

    yildiz eklendi

---
+++ src/giriş/yildiz.java
@@ -0,0 +1,23 @@
+package giriş;
+import java.util.Scanner;
+
+public class yildiz {
+    public static void main(String[] args) {
+        Scanner klavye = new Scanner(System.in);
+        System.out.println("Bir Sayı Giriniz :");
+        int n = klavye.nextInt();
+
+        for (int i = 0; i <= n ; i++) {
+            for (int j = 0; j < (n - i); j++) {
+                System.out.print(" ");
+            }
+            for (int k = 1; k <= (2 * i + 1); k++) {
+                System.out.print("*");
+            }
+            System.out.println(" ");
+        }
+
+
+    }
+}
+
